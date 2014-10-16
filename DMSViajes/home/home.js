4cdcf85d05dd0f463239b282f387110ee69893e7
Miguel AML <mangelmontero@hotmail.com>
2014-10-16 16:00:50 +0200

    home

---
+++ DMSViajes/home/home.js
@@ -0,0 +1,15 @@
+var goButton = document.getElementById('goButton');
+goButton.onclick = function(){
+    var myInput = document.getElementById('origin').value;
+    var myInput2 = document.getElementById('dateOr').value;
+    var myInput3 = document.getElementById('destination').value;
+    var myInput4 = document.getElementById('dateDest').value;
+    window.localStorage.removeItem("Origin");
+    window.localStorage.removeItem("dateOr");
+    window.localStorage.removeItem("Destination");
+    window.localStorage.removeItem("dateDest")
+    window.localStorage.setItem("Origin", myInput);
+    window.localStorage.setItem("dateOr", myInput2);
+    window.localStorage.setItem("Destination", myInput3);
+    window.localStorage.setItem("dateDest", myInput4);
+}
