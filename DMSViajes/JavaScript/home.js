a59c60ab6520e03ae5abc9ea949bcc672e301550
Miguel AML <mangelmontero@hotmail.com>
2014-10-24 12:16:34 +0200

    form

--- DMSViajes/JavaScript/home.js
+++ DMSViajes/JavaScript/home.js
@@ -1,3 +1,5 @@
+//function to add form content to the localStorage:
+
 /*var goButton = document.getElementById('goButton');
 goButton.onclick = function(){
     var myInput = document.getElementById('origin').value;
@@ -14,12 +16,44 @@ goButton.onclick = function(){
     window.localStorage.setItem("dateDest", myInput4);
 }*/
 
-var moreDest = document.getElementById("destB");
-destB.onclick = function moreDest (argument) {
-    var inputDestB = document.getElementById("inputDestB");
-    var inputDateB = document.getElementById("inputDateB");
-    inputDestB.classList.remove("inputB");
-    inputDateB.classList.remove("inputB");
-}
+//functions to add a destination in the form
+
+function moreDest () {
+    var inputDestB = document.getElementById('inputDestB');
+
+     if (inputDestB.className == 'inputB')
+        {inputDestB.classList.remove('inputB')
+    }
+     else {inputDestB.classList.add('inputB')};
+};
+
+function moreDest2 () {
+    var inputDestB = document.getElementById('inputDateB');
+
+     if (inputDateB.className == 'inputB')
+        {inputDateB.classList.remove('inputB')
+    }
+     else {inputDateB.classList.add('inputB')};
+};
+
+destB = document.getElementById('destB')
+destB.onclick = moreDest
 
 
+//Trying to unify the previous function:
+
+/*function moreDest (element,class) {
+    var element = document.getElementById(ID);
+     if (element.className == class)
+        {element.classList.remove(class)
+    }
+     else {element.classList.add(class)};
+};
+
+//var inputDestB = document.getElementById('inputDestB');
+//var inputDateB = document.getElementById('inputDateB');
+var destB = document.getElementById('destB')
+var ID = 'inputDestB'
+destB.onclick = moreDest (inputDestB,'inputB');
+var ID = 'inputDateB'
+destB.onclick = moreDest (inputDateB,'inputB');*/
